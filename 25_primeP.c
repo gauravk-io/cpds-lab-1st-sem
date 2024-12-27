af8d7ec10ac13f5fde86b5ed3445be1482636165
Gaurav <gauravkr7265@gmail.com>
2024-12-27 22:32:37 +0530

    solved Q-25

--- 25_primeP.c
+++ 25_primeP.c
@@ -3,6 +3,36 @@
 void main(){
 
             // Program in C to print n Prime Number.
+            #include <stdio.h>
+
+int isPrime(int num) {
+    if (num < 2) return 0; // Not prime
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0; // Divisible by another number
+    }
+    return 1; // Prime
+}
+
+void printNPrimes(int n) {
+    int count = 0, num = 2;
+    while (count < n) {
+        if (isPrime(num)) {
+            printf("%d ", num);
+            count++;
+        }
+        num++;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter the number of prime numbers to print: ");
+    scanf("%d", &n);
+    printNPrimes(n);
+    return 0;
+}
+
     
     
 }
